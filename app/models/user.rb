00538dcae21e392550a2ec143a191eaca739d994
Dave <dave.gagnat@gmail.com>
2020-12-21 17:28:21 -0600

    Added UserAuth to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,31 @@ class User < ApplicationRecord
     primary_key: :id,
     foreign_key: :author_id,
     class_name: :Article
+
+
+  # Main Methods
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end 
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
 end
