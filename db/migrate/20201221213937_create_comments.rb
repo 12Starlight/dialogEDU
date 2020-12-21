7126c9281b3f1c6ceb48c8cf213ac52ffb95ca47
Dave <dave.gagnat@gmail.com>
2020-12-21 15:57:34 -0600

    Added Comments migration

--- db/migrate/20201221213937_create_comments.rb
+++ db/migrate/20201221213937_create_comments.rb
@@ -1,8 +1,12 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      
+      t.text :body, null: false
+      t.integer :article_id, null: false
+
       t.timestamps
     end
+
+    add_index :comments, :article_id
   end
 end
