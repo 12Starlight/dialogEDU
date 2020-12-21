f397ab5639f40a5a328bc9bcc7ff72f9a4e90ea6
Dave <dave.gagnat@gmail.com>
2020-12-21 15:50:56 -0600

    Created Articles table

--- db/migrate/20201221213916_create_articles.rb
+++ db/migrate/20201221213916_create_articles.rb
@@ -1,8 +1,14 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
+      t.string :title, null: false
+      t.integer :author_id, null: false 
+      t.text :body
 
       t.timestamps
     end
+
+    add_index :articles, :title
+    add_index :articles, :author_id
   end
 end
