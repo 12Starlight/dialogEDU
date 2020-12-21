64d1fc63507c2a69e3bda0209bfc4ed16bd9d694
Dave <dave.gagnat@gmail.com>
2020-12-21 17:40:12 -0600

    Added Comment model with validations and associations

---
+++ app/models/comment.rb
@@ -0,0 +1,13 @@
+class Comment < ApplicationRecord
+  validates :body, presence: true
+
+  belongs_to :author
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User 
+
+  belongs_to :article
+    primary_key: :id,
+    foreign_key: :article_id,
+    class_name: :Article 
+end
