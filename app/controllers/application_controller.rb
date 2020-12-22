eeb34fed96f23f40b629b4e5d52010ed71e58fd9
Dave <dave.gagnat@gmail.com>
2020-12-21 18:16:30 -0600

    Built ApplicationkController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+
+  # Main Methods
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def require_logged_in
+    unless current_user
+      render json: {base: ['Invalid username and/or password']}, status: 401
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
 end
